f2387b0621bacb3ed5b643fe4f25e4eb54d17cd8
Brayan Ceron <brayan0np@gmail.com>
2025-08-18 19:19:02 -0500

    add AppTextArea.tsx component

---
+++ Frontend/src/components/inputs/AppTextArea.tsx
@@ -0,0 +1,63 @@
+type AppInputProps = {
+    value: string,
+    id: string,
+    name: string,
+
+    options?: OptionsInput,
+
+    [key: string]: any,
+}
+
+export type OptionsInput = {
+    label?: string,
+    placeholder?: string,
+    icon?: string,
+    helper?: string,
+}
+const AppTextArea = ({ value, name, id, options: { label = '', placeholder = '', helper = '', icon = '' } = {}, ...attributes }: AppInputProps) => {
+
+    // console.log(label, placeholder,icon, helper)// warn
+    return (
+        <>
+            <div className="w-full">
+                {
+                    label ?
+                        <label className="label label-text" htmlFor={id}> {label} </label>
+                        : <></>
+                }
+
+                <div className="input-group">
+                    {
+                        icon ?
+                            <span className="input-group-text">
+                                <span className={`icon-[tabler--${icon}] text-base-content/80 size-5`}></span>
+                            </span>
+                            : <></>
+                    }
+
+                    <textarea
+                        value={value}
+                        id={id}
+                        name={name}
+                        {...attributes} // rows={rows}
+                        placeholder={placeholder}
+                        className="textarea" aria-label="Textarea"
+                    />
+                </div>
+
+                {
+                    helper ?
+                        <span className="label">
+                            <span className="label-text-alt">{helper}</span>
+                        </span>
+                        : <></>
+                }
+
+            </div>
+
+        </>
+    )
+
+}
+
+export default AppTextArea;
