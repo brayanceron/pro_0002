6e958bde9a335a875298543ef63e0592ea9c57ea
Brayan Ceron <brayan0np@gmail.com>
2025-09-16 21:08:39 -0500

    add song_auth_controller.py auth controller

---
+++ Backend/src/controllers/auth_controllers/song_auth_controller.py
@@ -0,0 +1,76 @@
+from src.controllers.auth import get_auth_user, whoami
+from src.db.database import DatabaseConnection #,get_connection
+
+# conn = get_connection()
+db = DatabaseConnection()
+
+
+def auth_get() :#{
+    # data, _ = get_auth_user()
+    data, _ = whoami() # data token is validated in auth middleware
+    if(data['id'] != 'admin') : return {"message" : "user must be admin"}, 403
+#}
+
+def auth_get_id(user_id_of_item : str) :#{
+    # data, _ = get_auth_user()
+    data, _ = whoami() # data token is validated in auth middleware
+    if data['id'] == 'admin' : return None
+    if(data['id'] != user_id_of_item) : return {'message' : "Forbiden"}, 403
+#}
+
+def auth_post(user_id_of_item : str) :#{
+    user_data, status = whoami();
+    # if (status != 200) : return {'message' : "Unauthorize"}, 401
+    if user_data['id'] == 'admin' : return None;
+    if(user_data['id'] != user_id_of_item) : return {'message' : f"User {user_id_of_item} don't have permission for adding songs to user{user_data['id']}"}, 403 
+#}
+
+def auth_put(user_id_of_item : str) :#{
+    user_data, _ = whoami() # data token is validated in auth middleware
+
+    if user_data['id'] == 'admin' : return None
+    if (user_id_of_item != user_data['id']) : return {"message" : "Forbiden"}, 403
+#}
+
+def auth_delete(user_id_of_item) :#{
+    user_data, _ = whoami() # data token is validated in auth middleware
+
+    if user_data['id'] == 'admin' : return None
+    if (user_id_of_item != user_data['id']) : return {"message" : "Forbiden"}, 403
+#}
+
+
+def auth_get_by_user(user_id_of_item : str) :#{
+    return auth_get_id(user_id_of_item)
+#}
+
+# def auth_post_song_1(user_id : str, entity_id : str, entity_name) :#{
+#     # conn = get_connection()
+#     try :#{
+#         cur = conn.cursor();
+#         cur.execute("""select * from %s  where id = %s and and user_id = %s""", [entity_name, entity_id, user_id]);
+#         rows = cur.fetchall()
+#         if(cur.rowcount == 0) : return {'message' : f"{entity_name} with id{entity_id} Forbiden" }, 403
+#     #}
+#     except :#{
+#         return {'message' : "Some went bad verifying authorization :("}, 500
+#     #}
+# #}
+
+def auth_post_song_(user_id : str, entity_id : str, entity_name) :#{
+    conn = db.get_connection() # conn = get_connection()
+    
+    try :#{
+        cur = conn.cursor();
+        cur.execute("""select user_id from %s  where id = %s;""", [entity_name, entity_id]);
+        row = cur.fetchone()
+        # if(cur.rowcount == 0) : return {'message' : f"{entity_name} with id{entity_id} Forbiden" }, 403
+        if(cur.rowcount == 0) : return {'message' : f"{entity_name} not found" }, 404
+        gotten_user_id = row[0]
+        if(gotten_user_id == 'admin') : return
+        if(gotten_user_id != user_id) : return {'message' : f"{entity_name} whit id {entity_id} Forbiden" }, 403
+    #}
+    except :#{
+        return {'message' : "Some went bad verifying authorization :("}, 500
+    #}
+#}
