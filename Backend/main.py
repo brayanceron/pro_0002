98b48e7c6105e6d44c0858d9a703d79a8078d1a6
Brayan Ceron <brayan0np@gmail.com>
2025-09-26 09:47:15 -0500

    add environ variable

--- Backend/main.py
+++ Backend/main.py
@@ -1,5 +1,9 @@
 from src.app import app
+from pathlib import Path
+import os
 
 if __name__ == '__main__' :#{
+    PROJECT_ROOT_PATH = Path(__file__).parent.resolve()
+    os.environ['PROJECT_ROOT_PATH'] = str(PROJECT_ROOT_PATH)
     app.run(port = 5000, debug = True);
 #}
