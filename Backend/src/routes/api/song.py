acae91d6036f406d8a1f3c5049ce5aba24b4025e
Brayan Ceron <brayan0np@gmail.com>
2025-10-13 14:32:38 -0500

    add song.py file

---
+++ Backend/src/routes/api/song.py
@@ -0,0 +1,116 @@
+from flask import Blueprint, request
+import src.controllers.song
+import src.controllers.auth
+# from collections import defaultdict
+
+keys : list = ['name', 'description', 'url', 'goal', 'user_id', 'available', 'duration' ]
+ext_keys : list = ['genders', 'senses', 'singers', 'languages', 'playlists']
+
+song_router = Blueprint('song_blueprint', __name__)
+
+@song_router.route('/')
+def get() :#{
+    return src.controllers.song.get(request.args.get('extended', False))
+#}
+
+@song_router.route('/<id>')
+def get_id(id) :#{
+    return src.controllers.song.get_id(id, request.args.get('extended', False))
+#}
+
+@song_router.route('/', methods = ['POST'])
+def post() :#{
+    values = request.values # stream= request.stream
+
+    form = dict(request.form)
+    form = {k : form.get(k) for k in keys} # extract necesary keys
+    form = {**form, **{k : values.getlist(k) for k in ext_keys}} # extract list keys
+    form = {**form, **{k :  [v for v in (form.get(k) or []) if v] for k in ext_keys}} # format(delete empty values) list keys
+
+    del form['duration']
+    if form['available'] == None : del form['available']
+
+
+    image_file = request.files.get('image')
+    music_file = request.files.get('file')
+
+    print(form)
+    print(form)
+    # return {'message' : "Deliver error"}, 400
+    return src.controllers.song.post(**form, image_file = image_file, music_file = music_file)
+#}
+
+@song_router.route('/<id>', methods = ['PUT'])
+def put(id : str) :#{
+    values = request.values # stream= request.stream
+
+    form = dict(request.form)
+    form = {k : form.get(k) for k in keys} # extract necesary keys
+    form = {**form, **{k : values.getlist(k) for k in ext_keys}} # extract list keys
+    form = {**form, **{k :  [v for v in (form.get(k) or []) if v] for k in ext_keys}} # format(delete empty values) list keys
+
+    del form['url']
+    del form['duration']
+    if form['available'] == None : del form['available']
+
+    image_file = request.files.get('image')
+
+    # print(form)
+    result = src.controllers.song.put(id, **form, image_file = image_file)
+    print(f"{result=}")
+    return result
+    # return src.controllers.song.put(id, **form, image_file = image_file)
+#}
+
+
+@song_router.route('/by/user/<user_id>')
+def get_by_user(user_id) :#{
+    return src.controllers.song.get_by_user(user_id, request.args.get('extended', False))
+#}
+
+
+@song_router.route('/by/gender/<gender_id>')
+def get_by_gender(gender_id) :#{
+    return src.controllers.song.get_by_gender(gender_id)
+#}
+
+@song_router.route('/by/playlist/<playlist_id>')
+def get_by_playlist(playlist_id) :#{
+    return src.controllers.song.get_by_playlist(playlist_id)
+#}
+
+@song_router.route('/by/sense/<sense_id>')
+def get_by_sense(sense_id) :#{
+    return src.controllers.song.get_by_sense(sense_id)
+#}
+
+@song_router.route('/by/singer/<singer_id>')
+def get_by_singer(singer_id) :#{
+    return src.controllers.song.get_by_singer(singer_id)
+#}
+
+@song_router.route('/by/language/<language_id>')
+def get_bay_language(language_id) :#{
+    return src.controllers.song.get_by_language(language_id)
+#}
+
+
+@song_router.route('/generate', methods = ['POST'])
+def generate() :#{
+    ks = ['genders', 'senses', 'singers', 'languages']
+    form = dict(request.get_json())
+    form = {k : form.get(k, [] if k in ks else 0 ) for k in ['goal', 'user_id', *ks]} # extract necesary keys
+    # print(form)
+    form['user_id'] = "0f12951ac8cf486ba7a281d64fe311f7"
+    return src.controllers.song.generate(**form) 
+#}
+
+@song_router.before_request
+def auth_middleware() :#{
+    src.controllers.auth.auth_middleware()
+#}
+
+# @song_router.teardown_app_request
+# def teardown_db(err) :#{
+#     src.controllers.song.teardown_db()
+# #}
