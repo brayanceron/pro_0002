c716514c549be9e66069b2c69731fcff672effe1
Brayan Ceron <brayan0np@gmail.com>
2025-10-15 14:43:03 -0500

    update userFormData.ts hook

--- Frontend/src/hooks/useFormData.ts
+++ Frontend/src/hooks/useFormData.ts
@@ -1,25 +1,26 @@
 import { useState, } from "react";
 
-function useFormData<T>(fields: T): { data: T, formData: FormData, onChange: (event: any) => void, onChangeFile: (event: any) => void, onChangeMultipleSelect: (event: any) => void } {
+function useFormData<T>(fields: T, keysAreArray : string[] = []): { data: T, formData: FormData, onChange: (event: any) => void, onChangeFile: (event: any) => void, onChangeMultipleSelect: (event: any) => void } {
     const fd = new FormData()
     for (const key in fields) { // Object.keys()
         const value = fields[key]
-        fd.append(key, value as any)
+        if(Array.isArray(value)) value.forEach(item => fd.append(key, item as any)) //INFO it's solution to song form problem (method PUT)
+        else fd.append(key, value as any)
     }
 
     const [formData, setData] = useState<FormData>(fd)
     const [data, setFmtData] = useState<T>(fields)
 
     function onChange(event: any) {
-        formData.set(event.target.name, event.target.value)
         const newFormData = formData
+        newFormData.set(event.target.name, event.target.value)
         setData(newFormData)
         formatData()
     }
 
     function onChangeFile(event: any) {
-        formData.set(event.target.name, event.target.files[0])
         const newFormData = formData
+        newFormData.set(event.target.name, event.target.files[0])
         setData(newFormData)
         formatData()
     }
@@ -27,26 +28,24 @@ function useFormData<T>(fields: T): { data: T, formData: FormData, onChange: (ev
     function onChangeMultipleSelect(event: any) {
         const values = Array.from(event.target.selectedOptions, (option: any) => option.value);
         const key = event.target.name;
-        
-        formData.delete(key)
-        values.forEach((item: string) => { formData.append(key, item) })
-        formData.append(key, '')
-        formData.append(key, '')
-        
+
         const newFormData = formData
+        newFormData.delete(key)
+        values.forEach((item: string) => { newFormData.append(key, item) })
+        
         setData(newFormData)
         formatData()
     }
-
+    
     function formatData() {
         const obj: any = {}; // let obj : T | {} = {};
-        
-        formData.forEach((value, key) => {
+        formData.forEach((value : any, key) => {
             const keyValues = formData.getAll(key);
-            if (keyValues.length > 1) { obj[key] = keyValues.filter(cadena => cadena !== ""); }
-            else obj[key] = value;
-        });
 
+            if(keysAreArray.includes(key)) obj[key] = keyValues; // obj[key] = keyValues.filter(cadena => cadena !== "");
+            else obj[key] = value; // else obj[key] = keyValues[0]
+        });
+        console.log(obj)
         setFmtData(obj)
     }
 
