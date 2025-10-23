a82140c57a488f08f7a51adf112870ef5ecc2c95
brayanceron <brayan0np@gmail.com>
2025-10-22 19:22:48 -0500

    update PlayListProvider.tsx | change store playlist, from localstorage to database

--- Frontend/src/context/PlayListProvider.tsx
+++ Frontend/src/context/PlayListProvider.tsx
@@ -1,42 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { PlayListContext, type PlayListContextType, type BaseOptional } from "./PlayListContext";
-import type { SongModel } from "../models/SongModel";
-
-
-const defaultPlayList: SongModel[] = [
-    {
-        "description": "",
-        "gender_id": "33028177a27d4df9bec25aaa2ca65f27",
-        "goal": "5.00",
-        "id": "a478e2873741474d94bb6edb7fe4dfea",
-        "image": "",
-        "name": "Por mil noches - Airbag",
-        "url": "https://www.youtube.com/watch?v=ANs8-1iYkww",
-        "user_id": "8622e06b-65dd-11f0-94d0-089798b6be9f"
-    },
-    {
-        "description": "",
-        "gender_id": "33028177a27d4df9bec25aaa2ca65f27",
-        "goal": "5.00",
-        "id": "4c716558c52547ad81cc2a2f643c8d9b",
-        "image": "",
-        "name": "Jósean Log - Beso",
-        "url": "https://www.youtube.com/watch?v=ntdwWKaGaPQ",
-        "user_id": "8622e06b-65dd-11f0-94d0-089798b6be9f"
-    },
-    {
-        "description": "",
-        "gender_id": "33028177a27d4df9bec25aaa2ca65f27",
-        "goal": "5.00",
-        "id": "803888625bb0478f8f72efaca4e5be5e",
-        "image": "",
-        "name": "Diamante Electrico - Casino",
-        "url": "https://www.youtube.com/watch?v=HZb85wigeQc",
-        "user_id": "8622e06b-65dd-11f0-94d0-089798b6be9f"
-    },
-]
+import useFetch from "../hooks/useFetch";
+import { AuthContext } from "./AuthContext";
+// import type { SongModel } from "../models/SongModel";
 
 const PlayListProvider = ({ children }: { children: any }) => {
+    const {user} = useContext(AuthContext)
 
     function setPlayList(newData: BaseOptional) {
         setPlayListState(beforeData => { return { ...beforeData, ...newData } });
@@ -46,38 +15,15 @@ const PlayListProvider = ({ children }: { children: any }) => {
         setPlayListState(beforeData => { return { ...beforeData, currentIndex: newIndex } });  // setPlayListState({ ...playList, currentIndex: newIndex });
     }
 
-    const saveInLocalStorage = (data: SongModel[]) => {
-        /* if (data.length == 0) return; */
-        localStorage.setItem('playlist', JSON.stringify(data));
-    }
-    // const clearPLofLocalStorage = () =>{ localStorage.removeItem('playlist'); }
-
-    // const [playList, setPlayListState] = useState<PlayListContextType>({ playList: defaultPlayList, isLoading: false, error: null, setPlayList, currentIndex: 0, setCurrentIndex });
     const [playList, setPlayListState] = useState<PlayListContextType>({ playList: [], isLoading: false, error: null, setPlayList, currentIndex: 0, setCurrentIndex });
+    const {data, isLoading, error, res} = useFetch(`http://localhost:5000/api/song/get_generated_playlists/${user?.id}`)
 
-    useEffect(() => {
+    useEffect(()=>{
         // INFO isLoading should start in True
         // TODO validate playlist json has correct format
-        // reding in local storage
-        const strPL = localStorage.getItem('playlist');
-        console.log("###################");
-        if (strPL !== null) {
-            const jsonPL = JSON.parse(strPL);
-            console.log("jsonPL", jsonPL)
-            if (jsonPL.length > 0) {
-                console.log("Applaying jsonPL...")
-                setPlayListState(beforeData => { return { ...beforeData, playList: jsonPL } })
-            }
-        }
-        // BUG when user log out, must delete palylist, becouse if other user log in could see songs before user  
-    }, []);
-
-
-    // useEffect(()=>{ saveInLocalStorage(playList); },[playList]);
-    useEffect(() => {
-        if (playList.isLoading || playList.error) return
-        saveInLocalStorage(playList.playList);
-    }, [playList]);
+        if(isLoading || error || !res || !res.ok) return;
+        if(data && data['playlist']) setPlayListState(beforeData => { return { ...beforeData, playList: data['playlist'] } });
+    }, [data]);
 
     return (
         <PlayListContext.Provider value={{ ...playList }}>
