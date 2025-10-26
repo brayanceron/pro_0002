7522c3976e6ad3406efb53edab3dc0b25f2bb8c5
brayanceron <brayan0np@gmail.com>
2025-10-26 16:48:01 -0500

    update GenerationForm.tsx form

--- Frontend/src/components/forms/GenerationForm.tsx
+++ Frontend/src/components/forms/GenerationForm.tsx
@@ -3,7 +3,7 @@ import { GoalInput } from '../inputs/GoalInput';
 import AppButton from '../buttons/AppButton';
 import { useFormData } from '../../hooks/useFormData';
 import { usePost, type reqProps } from '../../hooks/usePost';
-import { useContext } from 'react';
+import { useContext, /* useEffect, useState */ } from 'react';
 import { AuthContext } from '../../context/AuthContext';
 
 type GenerationFormsFields = { // interface GenerationFormsFields  {
@@ -17,23 +17,31 @@ type GenerationFormsFields = { // interface GenerationFormsFields  {
 type ComponentProps = {
     values?: GenerationFormsFields,
     callback: (params: reqProps) => void,
+    saveArg?: boolean,
 }
 
 const emptyFields = { genders: [], senses: [], singers: [], languages: [], goal: 0, user_id: '' };
 const keyThatAreArrays = ['gender', 'sense', 'singer', 'language'];
 
 
-export const GenerationForm = ({ values = emptyFields, callback }: ComponentProps) => {
-    const {user} = useContext(AuthContext)
+export const GenerationForm = ({ values = emptyFields, callback, saveArg = true }: ComponentProps) => {
+    const { user } = useContext(AuthContext)
     values.user_id = user?.id || ''
-    let { data, onChangeMultipleSelect, onChange, /* formData, setAllFields, */ } = useFormData<GenerationFormsFields>(values, keyThatAreArrays.map(i => i + 's'));
-    const { sendReq } = usePost(data, "http://localhost:5000/api/song/generate", callback); // const { sendReq } = usePost(formData, "http://localhost:5000/api/song/generate", callback);
+
+    let { data, onChangeMultipleSelect, onChange } = useFormData<GenerationFormsFields>(values, keyThatAreArrays.map(i => i + 's'));
+    const { sendReq, isLoading } = usePost(data, `http://localhost:5000/api/song/generate?save=true'}`, callback);
+
     const onSubmit = () => { sendReq(); }
 
-    /* useEffect(() => {
-        console.log("values ->", values);
-        setAllFields(values);
-    }, [values]) */
+    /* //TODO (this for later) : this save playlist data only if the form data changes, it for avoid save repeated generated playlists
+    const [save, setSave] = useState(false); // it starts in false because in beginning both values and data are equal
+    const { sendReq, isLoading } = usePost(data, `http://localhost:5000/api/song/generate?${save ? 'save=true' : ''}`, callback); // const { sendReq } = usePost(formData, "http://localhost:5000/api/song/generate", callback);
+
+    useEffect(() => {
+        if (!saveArg) return;
+        if (compareData(values, data)) { setSave(false); }
+        else { setSave(true); }
+    }, [data]); */
 
     return (
         <>
@@ -52,7 +60,7 @@ export const GenerationForm = ({ values = emptyFields, callback }: ComponentProp
 
                     <GoalInput key="1" value={data.goal} name="goal" id="goal" label="Score" onChange={onChange} />
 
-                    <AppButton text="Generate" onClick={onSubmit} />
+                    <AppButton text="Generate" onClick={onSubmit} isLoading={isLoading} />
                 </>
 
             }
@@ -60,3 +68,23 @@ export const GenerationForm = ({ values = emptyFields, callback }: ComponentProp
 
     );
 }
+
+//INFO - This function compares two GenerationFormsFields objects to see if they are equal
+
+/* function compareData(obj1: GenerationFormsFields, obj2: GenerationFormsFields): boolean {
+    // if any is null or undefined
+    if (obj1 == null || obj2 == null) return false;
+    const keys1 = Object.keys(obj1);
+    const keys2 = Object.keys(obj2);
+
+    // if it have different keys
+    if (keys1.length !== keys2.length) return false;
+
+    for (const key of keys1) {
+        if (!keys2.includes(key)) return false;
+        if (JSON.stringify(obj1[key as keyof GenerationFormsFields]) !== JSON.stringify(obj2[key as keyof GenerationFormsFields])) {
+            return false;
+        }
+    }
+    return true;
+} */
