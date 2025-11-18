6691bd278c893ea30b662fb2429b914bee28bb4c
brayanceron <brayan0np@gmail.com>
2025-11-18 18:41:57 -0500

    update YT.tsx | improve performance

--- Frontend/src/components/player/Yt.tsx
+++ Frontend/src/components/player/Yt.tsx
@@ -1,16 +1,13 @@
-import { useEffect, useRef, useState, useImperativeHandle, forwardRef } from 'react';
+import { useContext, useEffect, useRef, useState,/*  useImperativeHandle, forwardRef */ } from 'react';
 import { ProgressBarPlayer } from './ProgressBarPlayer';
-import { getYouYubeVideoId } from '../../utils/urls';
+import { getYouYubeVideoId, isValidYouTubeUrl } from '../../utils/urls';
+import { PlayListContext } from '../../context/PlayListContext';
 
 interface Props {
     url: string,
-    index: string,
-}
-
-export interface YouTubePlayerHandle {
-    play: () => void;
-    pause: () => void;
-    getVideoData: () => any;
+    playing: boolean,
+    onFinishSong : () => void,
+    setPlaying: (val: boolean) => void,
 }
 
 declare global {
@@ -20,87 +17,77 @@ declare global {
     }
 }
 
-/* const getVideoId = (url: string): string | null => {
-    // if(!url.includes("youtube") ) return null
-    const regex = /(?:youtube\.com\/watch\?v=|youtu\.be\/)([\w-]{11})/;
-    const match = url.match(regex);
-    return match ? match[1] : null;
-}; */
-
-export const YouTubePlayer = forwardRef<YouTubePlayerHandle, Props>(({ url, index }, ref) => {
+//TODO what happen when video is paused for slow connection?
+//TODO verify if url is a youtube url
+export const YouTubePlayer = ({ url, playing, setPlaying, onFinishSong } : Props) => {
+    const { currentIndex } = useContext(PlayListContext);
 
     const playerRef = useRef<HTMLDivElement>(null);
     const playerInstance = useRef<any>(null);
-    const [playerState, setPlayerState] = useState(-1);
+    const [error, setError] = useState<boolean>(!isValidYouTubeUrl(url));
+    const [duration, setDuration] = useState(0)// const [duration, setDuration] = useState(-2)
+    const [playerState, setPlayerState] = useState<number | null>(null);
 
-    const [playing, setPlaying] = useState(false)
-    const [error, setError] = useState(false)
-    const [isLoadingSrc, setIsLoadingSrc] = useState(true)
-    const [duration, setDuration] = useState(0)
 
-    const play = () => { if (!playerInstance.current) return; setPlaying(true); }
-    const pause = () => { if (!playerInstance.current) return; setPlaying(false); }
-    useImperativeHandle(ref, () => ({ play, pause, getVideoData }));
+    useEffect(() => { exec(); }, [playing]);
+    useEffect(() => { 
+        if(error) return;
+        if (playerState == 0 ) { onFinishSong(); }
+        if (playerState == 1 ) { setError(false);} // if (status == 1 ) { setPlaying(true); setError(false);}
+        setterDuration();
+    }, [playerState]);
 
-    {/* PROGRESS SECTION */ }
-    function see() {
-        console.log("$$$$$$$$$$$$$")
-        console.log("YT =>", url)
-        console.log("isLoadingSrc = ", isLoadingSrc)
-        console.log("playerInstance = ", playerInstance)
-        console.log("playing = ", playing)
-        console.log("$$$$$$$$$$$$$")
-    }
-
-    useEffect(() => { exec(); see(); }, [playing]);
-    useEffect(() => { exec(); }, [isLoadingSrc]);
-    // useEffect(() => { rep.current!.src = url; }, [index]);
-    // useEffect(() => { loadStart() }, [index]);
     useEffect(() => {
-        loadStart()
-        setTimeout(() => { onLoaded() }, 200)
-        // onLoaded()
-        see()
+        if(!error) return;
+        setDuration(0);
+        setterDuration();
+        setPlayerState(null);
+        if(playerInstance.current && playerInstance.current.pauseVideo) playerInstance.current.pauseVideo();
+    }, [error]);
+    
+    useEffect(() => {
+        console.log("INDEX :: ", currentIndex)
+        setError(!isValidYouTubeUrl(url));
+        setDuration(0)
+        exec()
 
-    }, [index]);
-    // useEffect(() => { see() }, [playing]);
+        const videoId = isValidYouTubeUrl(url) ? getYouYubeVideoId(url) : null;
+        if (!videoId || !playerInstance.current || !playerInstance.current.loadVideoById) return; // if (!videoId || !playerRef.current || !playerInstance || !playerInstance.current) return;
+        playerInstance.current.loadVideoById(videoId)
+        setterDuration();
+        exec()
+    }, [currentIndex, url]);
+    // }, [currentIndex]) //[currentIndex, url]
 
 
-
-
-    const exec = () => { // console.log("playing=", playing, " isLoading=", isLoading)
-        // console.log("isLoadingSrc = " ,isLoadingSrc)
-        // console.log("playerInstance = " ,playerInstance)
-        if (!playerInstance.current || !playerInstance || !playerInstance.current.getDuration) return;
-        if (isLoadingSrc == true) return;
+    const exec = () => {
+        if(error) return;
+        if (!playerInstance.current || !playerInstance || !playerInstance.current.pauseVideo || !playerInstance.current.playVideo) return; // if (!playerInstance.current || !playerInstance || !playerInstance.current.getDuration) return;
 
         if (playing) { playerInstance.current.playVideo(); }
         else { playerInstance.current.pauseVideo(); }
-        //  if (playing) { rep.current!.play(); }
-        //  else { rep.current!.pause(); }
     }
 
-    const onLoaded = () => {
-        // if(!playerInstance.current) return setIsLoadingSrc(true);
-        if (!playerInstance.current || !playerInstance || !playerInstance.current.getDuration) return setIsLoadingSrc(true);
-        setIsLoadingSrc(false);
+    const setterDuration = () => {
+        if(error) return;
+        if(playerInstance.current && playerInstance.current.getDuration) { setDuration(playerInstance.current.getDuration()); }
     }
-    const loadStart = () => {
-        // if(!playerInstance.current) return setIsLoadingSrc(true);
-
-        if (!playerInstance.current || !playerInstance || !playerInstance.current.getDuration) return setIsLoadingSrc(true);
-        setIsLoadingSrc(true);
-    }
-
+    /* 
     const getVideoData = () => {
         if (!playerInstance.current || !playerInstance || !playerInstance.current.getDuration) return null;
         // console.log("INFO>>>", playerInstance.current.getVideoData());
         return playerInstance.current.getVideoData();
     }
+    */
+
+    const onChangeProgress = (newVal : number) => {
+        if(!playerInstance.current || !playerInstance || !playerInstance.current.seekTo) return;
+        playerInstance.current.seekTo(newVal, true);
+    }
 
     useEffect(() => {
-        // const videoId = isYt()? getVideoId(url) : getVideoId("https://www.youtube.com/watch?v=EfdsfdsfsaI");
-        const videoId = isYt() ? getYouYubeVideoId(url) : getYouYubeVideoId("https://www.youtube.com/watch?v=videofalsoo");
+        setError(!isValidYouTubeUrl(url));
+        const videoId = isValidYouTubeUrl(url) ? getYouYubeVideoId(url) : getYouYubeVideoId("https://www.youtube.com/watch?v=videofalsoo"); // const videoId = isYt()? getVideoId(url) : getVideoId("https://www.youtube.com/watch?v=EfdsfdsfsaI");
         if (!videoId || !playerRef.current) return;
 
         const onYouTubeIframeAPIReady = () => {
@@ -112,19 +99,13 @@ export const YouTubePlayer = forwardRef<YouTubePlayerHandle, Props>(({ url, inde
                 },
                 events: {
                     onReady: () => {
-                        onLoaded()
-                        setDuration(playerInstance.current.getDuration())
+                        console.log("--- PLAYER READY");
+                        setterDuration();
                         setError(false)
                     },
-                    onStateChange: (event: any) => {
-                        setPlayerState(event.data)
-                    },
-                    onAutoplayBlocked: () => {
-                        setPlaying(false)
-                    },
-                    onError: () => {
-                        setError(true);
-                    }
+                    onStateChange: (event: any) => { setPlayerState(event.data); },
+                    onAutoplayBlocked: () => { setPlaying(false); },
+                    onError: () => { setError(true); },
                 },
             });
         };
@@ -134,75 +115,33 @@ export const YouTubePlayer = forwardRef<YouTubePlayerHandle, Props>(({ url, inde
             tag.src = 'https://www.youtube.com/iframe_api';
             document.body.appendChild(tag);
             window.onYouTubeIframeAPIReady = onYouTubeIframeAPIReady;
-        } else {
-            onYouTubeIframeAPIReady();
-        }
+        } 
+        else { onYouTubeIframeAPIReady();}
 
-        return () => {
-            if (playerInstance.current?.destroy) {
-                playerInstance.current.destroy();
-            }
+        return () => { 
+            if (playerInstance.current?.destroy) { playerInstance.current.destroy(); }
         };
-    }, [url]);
-
-    useEffect(() => {
-        console.log(">>>  playerState", playerState)
-        // console.log("isLoadingSrc", isLoadingSrc)
-        // console.log("playing", playing)
-        // console.log("duration = ", playerInstance.current.getDuration())
-        // if (playerState == -1 ) {  console.log("duration = ", playerInstance.current.getDuration()) } 
-        // if (playerState == 0 ) {  console.log("duration0 = ", playerInstance.current.getDuration()) } 
-        // if (playerState == 1 ) {  console.log("duration1 = ", playerInstance.current.getDuration()) } 
-        // if (playerState == 2 ) {  console.log("duration2 = ", playerInstance.current.getDuration()) } 
-        // if (playerState == 3 ) {  console.log("duration3 = ", playerInstance.current.getDuration()) } 
-        // if (playerState == 5 ) {  console.log("duration5 = ", playerInstance.current.getDuration()) } 
-        // if (playerState == -1 ) { loadStart() } 
-        // if (playerState == -1 ) { onLoaded() } 
-        // if (playerState === 0 ) {  } 
-        // if (playerState === 1 ) { onLoaded() } 
-        // if (playerState === 2 ) { onLoaded() } 
-        // if (playerState == 3 ) {  onLoaded() } 
-        // if (playerState === 4 ) {  } 
-        // if (playerState == 5 ) { onLoaded() } 
-        // onLoaded()
-        // if(playerInstance.current) onLoaded()
-
-
-    }, [playerState])
-
-
-    /* function getInfoVideo() {
-        playerInstance.current.getDuration()
-     } */
-
-
-    function onChangeProgress() { }
-    function isYt() { return url.includes("youtube.com") }
+    }, []);// [url]
 
     return (
         <>
             <div ref={playerRef} id="youtube-player" className='hidden' ></div>
-            <div>
                 {
-                    isYt() ?
-                        <ProgressBarPlayer
-                            key={url}
-                            url={url}
-                            duration={duration}
-                            playing={playing}
-                            isLoadingSrc={isLoadingSrc}
-                            onChangeProgress={onChangeProgress}
-                            error={error}
-                        />
-                        : <p>src video</p>
+                        error ? <p>Error loading youtube video</p> :
+                            <ProgressBarPlayer duration={duration} playing={playing} onChangeTime={onChangeProgress}/>
                 }
-            </div>
         </>);
 }
-);
 
 
 
+
+/* const getVideoId = (url: string): string | null => {
+    // if(!url.includes("youtube") ) return null
+    const regex = /(?:youtube\.com\/watch\?v=|youtu\.be\/)([\w-]{11})/;
+    const match = url.match(regex);
+    return match ? match[1] : null;
+}; */
 
 
 // ======================================================================
