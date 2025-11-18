cc0152313f17624f98d0b70fe3347de491df5102
brayanceron <brayan0np@gmail.com>
2025-11-17 19:50:39 -0500

    update ProgrssBarPlayer.tsx | optimize progress bar

--- Frontend/src/components/player/ProgressBarPlayer.tsx
+++ Frontend/src/components/player/ProgressBarPlayer.tsx
@@ -1,86 +1,95 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState, useRef } from "react";
 
-function ProgressBarPlayer({ duration, playing, isLoadingSrc, error, url, onChangeProgress }: { duration: number, isLoadingSrc: boolean, playing: boolean, error : boolean, url: string, onChangeProgress: (newValue: number) => void }) {
-    const [progress, setProgress] = useState<number>(0);
-    const [currentTime, setCurrentTime] = useState<number>(0);
+type Props = {
+    duration: number,
+    playing: boolean,
+    onChangeTime : (nreVal : number) => void,   // callback: cuando usuario mueve la barra
+}
 
-    const rate = 500;
-    let durationL = duration * 1000; // transform to milliseconds
+const ProgressBarPlayer = ({ duration, playing, onChangeTime }: Props) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+    const progressRef = useRef(0);   // progress, time in seconds
+    const lastFrameRef = useRef(0);
+    const animRef = useRef(0); // const animRef = useRef(null);
 
-    // useEffect(() => { adv(); })
-    useEffect(() => { 
-        console.log("isLoadingSrc=", isLoadingSrc, "playing = ", playing); 
-        adv(); 
-    })
-    // console.log("timesOut=", timesOut)^^^^
-    // },[currentTime])
+    // start / stop animation
+    useEffect(() => {
+        if (!duration || duration <= 0) { // it reset when duration is 0, it means song changed, it fix whe change song and duration not updated yet
+            if (progressRef.current && progressRef.current > 0) { progressRef.current = 0; }
+            if (inputRef.current) { inputRef.current.value = "0"; }
+            setDisplayTime(0);
+        }
 
+        if (!playing) {
+            cancelAnimationFrame(animRef.current);
+            return;
+        }
 
-    // let timesOut: number[] = [];
-    const [timesOut, setTimesOut] = useState<number | null | undefined>(undefined) // let timesOut: number | null | undefined = undefined;
-    function adv() {
-        if(error) return;
-        if (!playing) return;
-        if (isLoadingSrc) return;
+        
+        const tick = (now: any) => {
+            const delta = (now - lastFrameRef.current) / 1000; // ms->segundos
+            lastFrameRef.current = now;
+            progressRef.current += delta;
 
-        // console.log("timesOut=", timesOut)
-        if (timesOut) { setTimesOut(null); return; }
-        setTimesOut(setTimeout(() => {
-            setCurrentTime(currentTime + rate)
-            setProgress((currentTime / durationL!) * 100)
-        }, rate));
-    }
+            if (progressRef.current > duration) { progressRef.current = duration; }
+            if (inputRef.current) { inputRef.current.value = progressRef.current.toString(); } // actualización directa al DOM (sin re-render)
 
+            animRef.current = requestAnimationFrame(tick); /*  */
+        };
+
+        lastFrameRef.current = performance.now(); /* seconds passed from open tab  */
+        animRef.current = requestAnimationFrame(tick);
+
+        return () => cancelAnimationFrame(animRef.current);
+    }, [playing, duration]);
+
+    const handleChange = (e : any) => {
+        const val = Number(e.target.value);
+
+        progressRef.current = val;
+        setDisplayTime(val);
+        onChangeTime && onChangeTime(val);
+    };
+
+    const [displayTime, setDisplayTime] = useState(0);
 
     useEffect(() => {
-        console.log("CAMBIO LA DURACION POR ENDE CAMBIO LA CANCION")
-        if (timesOut === undefined) return;
-        setCurrentTime(0);
-        setProgress(0);
+        const id = setInterval(() => {
+            setDisplayTime(progressRef.current);
+        }, 250);
 
-        clearTimeout(timesOut || 100)
-        setTimesOut(null)
-        // timesOut = null;
-    // }, [duration])
-    }, [url])
+        return () => clearInterval(id);
+    }, []);
 
-
-    //  const onChangeProgress = (event: ChangeEvent) => {
-    const onChangeProgress_ = (event: React.ChangeEvent) => {
-        const ele = event.target as HTMLInputElement
-        // const totalTime = rep.current!.duration;
-
-        console.log("ele.value=", ele.value)
-
-        // rep.current!.currentTime = totalTime * (parseFloat(ele.value) / 100);
-        // rep.current!.currentTime = durationSeconds * (parseFloat(ele.value) / 100);
-        const newPercentProgress = ele.value;
-        const newCurrentTimeValue: number = duration * (parseFloat(newPercentProgress) / 100);
-
-        setProgress(parseInt(newPercentProgress));
-        setCurrentTime(newCurrentTimeValue)
-        onChangeProgress(newCurrentTimeValue)
-    }
-
-    function formatTime() {
-        const seconds: number = Math.trunc(currentTime / 1000) % 60;
-        const minutes: number = Math.trunc(currentTime / (1000 * 60));
+    function formatTime(time: number) {
+        const seconds: number = Math.trunc(time / 1000) % 60;
+        const minutes: number = Math.trunc(time / (1000 * 60));
         return `${minutes <= 9 ? 0 : ''}${minutes}:${seconds <= 9 ? 0 : ''}${seconds}`
+        // const seconds: number = Math.trunc(currentTime / 1000) % 60;
+        // const minutes: number = Math.trunc(currentTime / (1000 * 60));
     }
+    const format = (sec: number) => {
+        const m = Math.floor(sec / 60);
+        const s = Math.floor(sec % 60);
+        return `${String(m).padStart(2, "0")}:${String(s).padStart(2, "0")}`;
+    };
 
     return (
         <div className="py-1">
-            <input type="range"
-                className="range bg-transparent border-solid border-2"
-                aria-label="range"
-                value={progress}
-            // onChange={event => onChangeProgress_(event)}
-            onChange={onChangeProgress_}
-            />
-            <p className="text-xs">{formatTime()}</p>
-        </div>
 
+            <input
+                className="range bg-transparent border-solid border-2"
+                ref={inputRef}
+                type="range"
+                min={0}
+                max={duration}
+                step={0.01}
+                defaultValue={0}
+                onChange={handleChange}
+            />
+            <p className="text-xs">{format(displayTime)} / {formatTime(duration * 1000)}</p>
+        </div>
     );
 }
 
-export {ProgressBarPlayer}
+export { ProgressBarPlayer };
