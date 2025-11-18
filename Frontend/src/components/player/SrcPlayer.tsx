6b042f4ff0b7636aed1e5df132edd310deb5878b
brayanceron <brayan0np@gmail.com>
2025-11-18 15:31:01 -0500

    update SrcPlayer.tsx

--- Frontend/src/components/player/SrcPlayer.tsx
+++ Frontend/src/components/player/SrcPlayer.tsx
@@ -1,86 +1,68 @@
-import { useEffect, useRef, useState, useImperativeHandle, forwardRef } from "react";
+import {  useContext, useEffect, useRef, useState} from "react";
 import { ProgressBarPlayer } from "./ProgressBarPlayer";
+import { isValidSrcUrl } from '../../utils/urls';
+import { PlayListContext } from "../../context/PlayListContext";
 
 interface Props {
     url: string,
-    index: number,
+    playing: boolean,
+    setPlaying: (val: boolean) => void,
+    onFinishSong : () => void,
 }
 
-export interface HandleSrc {
-    play: () => void,
-    pause: () => void,
-}
 
 //TODO verify if url is a url supported by audio element or is url of backend
-// export function Src({ url }: { url: string }) {
-export const SrcPlayer = forwardRef<HandleSrc, Props>(({ url, index }, ref) => {
-    const [playing, setPlaying] = useState(false)
-    const [error, setError] = useState(false)
-    const [isLoadingSrc, setIsLoadingSrc] = useState(true)
+export const SrcPlayer = ({ url, playing, onFinishSong } : Props) => {
+    const { currentIndex } = useContext(PlayListContext);
+    const [error, setError] = useState(!isValidSrcUrl(url));
     const rep = useRef<HTMLAudioElement>(null);
+    const [duration, setDuration] = useState<number>(0);
+    
 
-    const play = () => { setPlaying(true); };
-    const pause = () => { setPlaying(false); };
-    useImperativeHandle(
-        ref, () => ({ play, pause, })
-    );
-
-
-    {/* PROGRESS SECTION */ }
-    console.log("SRC =>", url)
+    useEffect(() => { setError(!isValidSrcUrl(url)); }, [url]);
     useEffect(() => { exec(); }, [playing]);
-    useEffect(() => { exec(); }, [isLoadingSrc]);
-    useEffect(() => { rep.current!.src = url; }, [index]);  // useEffect(() => { rep.current!.src = url; }, [url]);
-    const exec = () => { // console.log("playing=", playing, " isLoading=", isLoading)
-        if (isLoadingSrc == true) return;
+    useEffect(() => { exec(); }, [duration]);
+    useEffect(() => { rep.current!.src = url; }, [currentIndex]);  // useEffect(() => { rep.current!.src = url; }, [url]);
+    useEffect(() => {
+        if(!error) return;
+        setDuration(0);
+    }, [error]);
+
+    const exec = () => {
+        if(error) return;
+        if (duration <= 0) return;
         if (playing) { rep.current!.play(); }
         else { rep.current!.pause(); }
     }
 
-    const onLoaded = () => { setIsLoadingSrc(false); }
-    const loadStart = () => { setIsLoadingSrc(true); }
     const onError = () => { setError(true); }
+    const onCanPlay =  () => { if (rep.current) { setDuration(rep.current.duration); } }
 
-
-    // const [progress, setProgress] = useState(0);
-
-    /* const onChangeProgress = (event: ChangeEvent) => {
-        const ele = event.target as HTMLInputElement
-        const totalTime = rep.current!.duration;
-
-        setProgress(parseInt(ele.value))
-        rep.current!.currentTime = totalTime * (parseFloat(ele.value) / 100);
-    } */
-
-
-    function onChangeProgress(newValue: number) {
-        pause();
-        rep.current!.currentTime = newValue;
-        play()
+    const onChangeProgress = (newVal : number) => {
+        console.log("New val : ", newVal)
+        if(!rep.current) return;
+        rep.current.currentTime = newVal;
     }
-    function isYt() { return url.includes("youtube.com") }
-
     return (
         <>
-
             <div>
                 {
-                    !isYt() ?
+                    error ? <p>Error loading src video</p> :
                         <ProgressBarPlayer
                             key={url}
-                            duration={rep.current?.duration || 0}
+                            duration={duration} // duration={rep.current?.duration || 0}
                             playing={playing}
+                            onChangeTime={onChangeProgress}
                         />
-                        : <p>Youtube video</p>
                 }
 
                 <audio
                     ref={rep}
                     src={url}
                     controls className="hidden w-full"
-                    onLoadStart={loadStart}
-                    onCanPlayThrough={onLoaded}
                     onError={onError}
+                    onEnded={ _ => onFinishSong()}
+                    onCanPlay={ _ => onCanPlay()} // onCanPlayThrough={onLoaded}
                 />
             </div>
 
@@ -88,5 +70,4 @@ export const SrcPlayer = forwardRef<HandleSrc, Props>(({ url, index }, ref) => {
     )
 
 }
-)
 
