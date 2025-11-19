5d959f7a6cf8fb85bf9efb4654da8c4faa491f0a
brayanceron <brayan0np@gmail.com>
2025-11-19 09:51:39 -0500

    update playing.tsx | fit background to player

--- Frontend/src/routes/Playing.tsx
+++ Frontend/src/routes/Playing.tsx
@@ -4,29 +4,25 @@ import { PlayListContext } from "../context/PlayListContext";
 import SongList from "../components/player/SongList";
 
 const Playing = () => {
-    // const { playList, isLoading, error } = useContext(PlayListContext);
     const { playList, isLoading, error, currentIndex } = useContext(PlayListContext);
     console.log("reendering all, route")
 
     return (
-        // <div className="w-full m-auto bg-black">
-            <div className="w-2/3 m-auto bg-black">
+        <>
             {
                 isLoading ? <p>Loading...</p> :
                     error ? <p>Error : {error.message}</p> :
-                        <>
-
-                            {/* <div className="w-2/3 bg-black"> */}
+                        <div className="w-2/3 m-auto">
+                            <div className="bg-black">
                                 <PlayerComponent /> {/* <PlayerComponent songs={["https://music.youtube.com/watch?v=w9jrC_oJQNM"]} /> */}
-                            {/* </div> */}
+                            </div>
                             
                             <div className="h-[350px] overflow-y-scroll">
                                 <SongList playList={playList} isLoading={isLoading} error={error} currentIndex={currentIndex} />
                             </div>
-                            
-                        </>
+                        </div>
             }
-        </div>
+        </>
     );
 }
 
