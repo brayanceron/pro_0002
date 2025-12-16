ac3c024100d75f605aa7916c1ad5ea8950323549
brayanceron <brayan0np@gmail.com>
2025-12-15 20:48:16 -0500

    update Youtube player | Yt.tsx

--- Frontend/src/components/player/Yt.tsx
+++ Frontend/src/components/player/Yt.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useRef, useState, useImperativeHandle, forwardRef, } from 'react';
+import { useContext, useEffect, useRef, /* useState,  */useImperativeHandle, forwardRef, useReducer, } from 'react';
 import { getYouYubeVideoId, isValidYouTubeUrl } from '../../utils/urls';
 import { PlayListContext } from '../../context/PlayListContext';
 
@@ -17,67 +17,67 @@ declare global {
     }
 }
 
-//TODO what happen when video is paused for slow connection?
-// BUG when there is error to playing song and player play next song, it pass 2 songs
-export const YouTubePlayer = forwardRef(({ url, playing, setPlaying, onFinishSong, onChangeStates } : Props, ref) => {
-    const { currentIndex, /* playList */ } = useContext(PlayListContext);
+interface PlayerStates {
+    error : boolean | null,
+    duration : number | null,
+    playerState : number | null,
+}
+interface ReduxPayload extends PlayerStates {
+    url? : string,
+}
+type ReduxAction = {
+    type : string,
+    payload : ReduxPayload, 
+}
 
+const reducer = (prevState : PlayerStates, action : ReduxAction ) : PlayerStates => {
+    const {duration, error, playerState, url} = action.payload;
+    switch(action.type){
+        case 'ERROR' : { return { playerState : null, error, duration } }
+        case 'PLAYERSTATE' : {
+            let newerror = error == null ? !isValidYouTubeUrl(url!) : error; // if error = null, calculate error with current state, else set sent error
+            if (playerState == 1 ) { newerror = false; } // if (status == 1 ) { setPlaying(true); setError(false);}
+            return {error : newerror, playerState, duration } 
+        }
+        case 'ONCHANGEURL' : { return {...prevState, error : !isValidYouTubeUrl(url!), duration } }
+        default : { return prevState; }
+    }
+}
+
+export const YouTubePlayer = forwardRef(({ url, playing, setPlaying, onFinishSong, onChangeStates } : Props, ref) => {
     const playerRef = useRef<HTMLDivElement>(null);
     const playerInstance = useRef<any>(null);
+    const { currentIndex } = useContext(PlayListContext);
 
-    const [error, setError] = useState<boolean>(!isValidYouTubeUrl(url));
-    const [duration, setDuration] = useState(0)// const [duration, setDuration] = useState(-2)
-    const [playerState, setPlayerState] = useState<number | null>(null);
+    const initialState : PlayerStates = {error : !isValidYouTubeUrl(url), duration : 0, playerState : null}; // const initialState : ReduxPayload = {error : null, duration : 0, playerState : null};
+    const [plState, dispatch] = useReducer(reducer, initialState);
 
-    useEffect(() => { exec(); }, [playing]);
-    useEffect(() => { 
-        if (playerState == 0 ) { if(!error) onFinishSong(); }
-        if (playerState == 1 ) { setError(false);} // if (status == 1 ) { setPlaying(true); setError(false);}
-        setterDuration();
-    }, [playerState]);
-
-    useEffect(() => {
-        // if(isValidYouTubeUrl(url)==false) return; //HACK
-        if(!error) return;
-        setterDuration();
-        setPlayerState(null);
-        if(playerInstance.current && playerInstance.current.pauseVideo) playerInstance.current.pauseVideo();
-    }, [error]);
-    
-    useEffect(() => {
-        if(error && isValidYouTubeUrl(url) == false) return; 
-        onChangeStates(error, duration, playerInstance.current && playerInstance.current.getCurrentTime ? playerInstance.current.getCurrentTime() : 0);
-    }, [error, duration, playing]);
-
-    
-    useEffect(() => {
-        // if(isValidYouTubeUrl(url)==false) return; //HACK
-        setError(!isValidYouTubeUrl(url));
-        exec()
-
-        const videoId = isValidYouTubeUrl(url) ? getYouYubeVideoId(url) : 'null';
-        // if (!videoId || !playerInstance.current || !playerInstance.current.loadVideoById) return; // if (!videoId || !playerRef.current || !playerInstance || !playerInstance.current) return;
-        if (!playerInstance.current || !playerInstance.current.loadVideoById) return; // if (!videoId || !playerRef.current || !playerInstance || !playerInstance.current) return;
-        playerInstance.current.loadVideoById(videoId)
-        setterDuration();
-        exec()
-    }, [currentIndex, url]);
-
-
-    const exec = () => {
+    const exec = () => { // if(!state) return;
         if (!playerInstance.current || !playerInstance || !playerInstance.current.pauseVideo || !playerInstance.current.playVideo) return; // if (!playerInstance.current || !playerInstance || !playerInstance.current.getDuration) return;
-        if(error) playerInstance.current.pauseVideo(); // if(error) return;
-        // if(error || !isValidYouTubeUrl(url)) playerInstance.current.pauseVideo(); // if(error) return;
-
+        if(plState.error) playerInstance.current.pauseVideo();// if(error || !isValidYouTubeUrl(url)) playerInstance.current.pauseVideo(); // if(error) return;
         if (playing) { playerInstance.current.playVideo(); }
         else { playerInstance.current.pauseVideo(); }
     }
 
-    const setterDuration = () => {
-        if(isValidYouTubeUrl(url)==false) return; //HACK
-        if(error) setDuration(-1); // if(error) return;
-        if(playerInstance.current && playerInstance.current.getDuration) { setDuration(playerInstance.current.getDuration()); }
+    const getDuration = () => { //FIXME - current error or new error???
+        if(isValidYouTubeUrl(url)==false) return -1;
+        if(playerInstance.current && playerInstance.current.getDuration) { return playerInstance.current.getDuration(); }
+        return -1;
     }
+    
+    useEffect(() => { exec(); }, [playing]);
+    
+    useEffect(() => {
+        if(plState.error && isValidYouTubeUrl(url) == false) return; 
+        onChangeStates(plState.error != null ? plState.error : false, plState.duration ? plState.duration : 0, playerInstance.current && playerInstance.current.getCurrentTime ? playerInstance.current.getCurrentTime() : 0);
+        if (plState.playerState === 0 && !plState.error) { onFinishSong(); }
+    }, [plState.playerState, plState.error, plState.duration]);
+    
+    useEffect(() => {
+        dispatch({type : 'ONCHANGEURL', payload : {error : null, duration : getDuration(), playerState : null, url}}) // dispatch payload without side-effect functions; run side-effects after dispatch
+        changeUrl(); // run loader to actually load new video
+    }, [currentIndex, url]);
+
     /* 
     const getVideoData = () => {
         if (!playerInstance.current || !playerInstance || !playerInstance.current.getDuration) return null;
@@ -85,9 +85,15 @@ export const YouTubePlayer = forwardRef(({ url, playing, setPlaying, onFinishSon
         return playerInstance.current.getVideoData();
     }
     */
+    const changeUrl = () =>{
+        const videoId = isValidYouTubeUrl(url!) ? getYouYubeVideoId(url!) : 'null';
+            // if (!videoId || !playerInstance.current || !playerInstance.current.loadVideoById) return; // if (!videoId || !playerRef.current || !playerInstance || !playerInstance.current) return;
+        if (!playerInstance.current || !playerInstance.current.loadVideoById) return; // if (!videoId || !playerRef.current || !playerInstance || !playerInstance.current) return;
+        playerInstance.current.loadVideoById(videoId)
+        exec()
+    }
 
     useEffect(() => {
-        setError(!isValidYouTubeUrl(url));
         const videoId = isValidYouTubeUrl(url) ? getYouYubeVideoId(url) : getYouYubeVideoId("https://www.youtube.com/watch?v=videofalsoo"); // const videoId = isYt()? getVideoId(url) : getVideoId("https://www.youtube.com/watch?v=EfdsfdsfsaI");
         if (!videoId || !playerRef.current) return;
 
@@ -101,15 +107,12 @@ export const YouTubePlayer = forwardRef(({ url, playing, setPlaying, onFinishSon
                 },
                 events: {
                     onReady: () => {
-                        console.log("--- PLAYER READY");
-                        setterDuration();
-                        setPlayerState(-1);
-                        setError(false)
+                        dispatch({type : 'PLAYERSTATE', payload : {error : false, duration : getDuration(), playerState : null, url}})
                         exec();
                     },
-                    onStateChange: (event: any) => { setPlayerState(event.data); },
+                    onStateChange: (event: any) => { dispatch({type : 'PLAYERSTATE', payload : {playerState : event.data, error : null, duration : getDuration(), url}}) },
                     onAutoplayBlocked: () => { setPlaying(false); },
-                    onError: () => { setError(true); },
+                    onError: () => { dispatch({type : 'ERROR', payload : {error : true, duration : getDuration(), playerState : null}}) },
                 },
             });
         };
@@ -121,12 +124,9 @@ export const YouTubePlayer = forwardRef(({ url, playing, setPlaying, onFinishSon
             window.onYouTubeIframeAPIReady = onYouTubeIframeAPIReady;
         } 
         else { onYouTubeIframeAPIReady();}
-        // exec();
 
-        return () => { 
-            if (playerInstance.current?.destroy) { playerInstance.current.destroy(); }
-        };
-    }, []);// [url]
+        return () => { if (playerInstance.current?.destroy) { playerInstance.current.destroy(); } };
+    }, []);
 
     useImperativeHandle(ref, () => ({
         onChangeProgress(newVal: number) {
@@ -139,13 +139,34 @@ export const YouTubePlayer = forwardRef(({ url, playing, setPlaying, onFinishSon
     }));
     return (
         <>
-            <div className={`w-full ${error ? 'hidden' : ''}`}>
+            <div className={`w-full ${plState.error ? 'hidden' : ''}`}>
                 <div ref={playerRef} id="youtube-player" className={`w-full`}></div>
             </div>
-                {
-                        error && <p>Error loading youtube video</p>
-                }
+            { plState.error && <p>Error loading youtube video</p>}
         </>);
 })
 // }
 
+
+
+
+/* const run0 = ({error : newError} : PlayerStates) =>{
+        if(playerInstance.current && playerInstance.current.pauseVideo && newError) playerInstance.current.pauseVideo();
+    }
+
+    const run1 = ({error : error_i, playerState : playerState_i} : PlayerStates) =>{
+        if (playerState_i == 0 ) { 
+            console.log("ADELANTANDO...")
+            console.log("playerState_i : ",playerState_i, " error_i : ",error_i)
+            if(!error_i) onFinishSong();
+        }
+    } */
+/* case 'PLAYERSTATE' : {
+            let newError = null;
+            if (playerState == 0 ) { if(!error) action.payload.onFinishSong!(); }
+            if (playerState == 1 ) { newError = false; } // if (status == 1 ) { setPlaying(true); setError(false);}
+            return {...prevState,error : newError !=null ? newError : prevState.error, playerState : playerState, duration } 
+        }
+        case 'SET' : {
+            return {error, duration, playerState} // return action.payload 
+        } */
