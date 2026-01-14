194fb0dfb1023b9712146fb0f6a3467014317458
brayanceron <brayan0np@gmail.com>
2026-01-14 14:16:51 -0500

    update SenseInput

--- Frontend/src/components/forms/SongForm/SenseInput.tsx
+++ Frontend/src/components/forms/SongForm/SenseInput.tsx
@@ -4,15 +4,15 @@ import { WrapMultipleSelect } from "./WrapMultipleSelect";
 export type SenseOptionsType = {
     id: string,
     name: string,
-    score: number,
+    score: { min : number, max: number }, // score: number,
 }
 
 //TODO change this file to inputs folder
-const SenseInput = ({ defaultValues, onChange }: { defaultValues: SenseOptionsType[], onChange: (selection: SenseOptionsType[]) => void }) => {
+const SenseInput = ({ defaultValues, onChange, isMultiple = false }: { defaultValues: SenseOptionsType[], onChange: (selection: SenseOptionsType[]) => void, isMultiple?: boolean }) => {
     const [selection, setSelection] = useState<SenseOptionsType[]>(defaultValues ? defaultValues : []);
 
     const onChangeRange = ({ /* name, */ score, id }: SenseOptionsType) => {
-        const updatedSelection = selection.map((item: SenseOptionsType) => { return item.id === id ? { ...item, score: score } : item; });
+        const updatedSelection = selection.map((item: SenseOptionsType) => { return item.id === id ? { ...item, score } : item; });
         if (updatedSelection) setSelection(updatedSelection);
     }
 
@@ -33,38 +33,38 @@ const SenseInput = ({ defaultValues, onChange }: { defaultValues: SenseOptionsTy
                     options.forEach((option: any) => {
                         const f: SenseOptionsType | undefined = selection.find((s) => s.id === option.value);
                         if (f) { newSelection.push({'id': f.id, 'name': f.name, 'score': f.score });}
-                        else{newSelection.push({'id': option.value, 'name': option.text, 'score': 0.00});} // default score
-                        // const { name, id, score } = f ? f : { name: option.text, id: option.value, score: 0.00 };
+                        else{newSelection.push({'id': option.value, 'name': option.text, 'score': {min: isMultiple ? 0 : -1, max: 0}});} // default score
                     });
                     setSelection(newSelection);
                 }}
             />
             <br />
-            <SensesRanges selectedSenses={selection} onChangeRange={onChangeRange} />
+            <SensesRanges selectedSenses={selection} onChangeRange={onChangeRange} isMultiple={isMultiple} />
         </>
     );
 }
 
-const SensesRanges = ({ selectedSenses, onChangeRange }: { selectedSenses: SenseOptionsType[], onChangeRange: (values: SenseOptionsType) => void }) => {
+const SensesRanges = ({ selectedSenses, onChangeRange, isMultiple }: { selectedSenses: SenseOptionsType[], onChangeRange: (values: SenseOptionsType) => void, isMultiple: boolean }) => {
     return (
         <>
             {
                 selectedSenses.map(({ name, score, id }: SenseOptionsType) => {
                     return (
                         <>
-                            <div className="flex align-middle justify-center items-center">
-                                <p className="mr-2 w-auto text-sm text-center">{name}</p>
+                            <div className="flex align-middle justify-center items-center mb-[3px]">
+                                <p className="mr-1 px-0 w-2/12 text-xs text-right">{name}</p>
                                 <input
                                     type="range"
-                                    className="range w-4/5"
+                                    className="range w-9/12 mx-0"
                                     aria-label="range"
-                                    defaultValue={score} // value={rangeValue}
-                                    onChange={(e) => onChangeRange({ name, id, score: parseFloat(e.target.value) })}
+                                    defaultValue={score.max}
+                                    value={score.max}
+                                    onChange={(e) => onChangeRange({ name, id, score: { min: isMultiple ? 0 : -1, max: parseFloat(e.target.value)} })}
                                     min={0}
                                     max={5}
                                     step={0.01}
                                 />
-                                <p className="text-xs text-gray-500 ml-2 w-auto">{score}</p>
+                                <p className="text-xs text-gray-500 ml-2 w-1/12">{score.max}</p>
                             </div>
                         </>
                     );
