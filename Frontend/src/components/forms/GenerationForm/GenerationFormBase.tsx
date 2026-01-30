fc9dbcece5b5cafdb12ca4c47b1e81dfa2887e4d
brayanceron <brayan0np@gmail.com>
2026-01-30 11:02:19 -0500

    format GenerationFormBase.tsx

--- Frontend/src/components/forms/GenerationForm/GenerationFormBase.tsx
+++ Frontend/src/components/forms/GenerationForm/GenerationFormBase.tsx
@@ -2,7 +2,7 @@ import { SenseInput, type SenseOptionsType, /* type SenseOptionsType */ } from "
 import { WrapMultipleSelect } from "../SongForm/WrapMultipleSelect";
 
 type Props = {
-    data?: GenerationFormsBaseFields,
+    defaultValues?: GenerationFormsBaseFields,
     onChange: (Event: any) => void,
 }
 export type GenerationFormsBaseFields = {
@@ -10,14 +10,12 @@ export type GenerationFormsBaseFields = {
     senses?: SenseOptionsType[], // senses?: string[],
     singers?: string[],
     languages?: string[],
-    // goal?: number,
-    // user_id?: string,
 }
 
-export const emptyFields : GenerationFormsBaseFields = { genders: [], senses: [], singers: [], languages: [] };
+export const emptyFields: GenerationFormsBaseFields = { genders: [], senses: [], singers: [], languages: [] };
 
-export const GenerationFormBase = ({ data = emptyFields, onChange }: Props) => {
-    data = {...emptyFields, ...data};
+export const GenerationFormBase = ({ defaultValues = emptyFields, onChange }: Props) => {
+    defaultValues = { ...emptyFields, ...defaultValues };
 
     const onChangeSense = (selection: any) => { onChange({ target: { name: 'senses', value: selection } }); }
     const onChangeMultipleSelect = (event: any) => {
@@ -25,25 +23,25 @@ export const GenerationFormBase = ({ data = emptyFields, onChange }: Props) => {
         const key = event.target.name;
         onChange({ target: { name: key, value: values } });
     }
-    const theKeys = Object.keys(data).map((item : string) => item.substring(0, item.length - 1)).filter( f => f != 'sense');
+    const theKeys = Object.keys(defaultValues).map((item: string) => item.substring(0, item.length - 1)).filter(f => f != 'sense');
 
     return (
         <>
             {
                 theKeys.map((item: string) => {
-                        return (
-                            <WrapMultipleSelect
-                                entity={item}
-                                values={data[`${item}s` as keyof GenerationFormsBaseFields] as string[] || []} // values={data[`${item}` as keyof GenerationFormsBaseFields] as string[] || []}
-                                onChangeMultipleSelect={onChangeMultipleSelect}
-                                key={item}
-                            />
-                        );
+                    return (
+                        <WrapMultipleSelect
+                            entity={item}
+                            values={defaultValues[`${item}s` as keyof GenerationFormsBaseFields] as string[] || []} // values={data[`${item}` as keyof GenerationFormsBaseFields] as string[] || []}
+                            onChangeMultipleSelect={onChangeMultipleSelect}
+                            key={item}
+                        />
+                    );
                 })
             }
 
             <div className="w-full shadow-md px-2 py-4 my-3 border-[1px] border-gray-200">
-                <SenseInput defaultValues={data.senses || []} onChange={onChangeSense} isMultiple={true} />
+                <SenseInput defaultValues={defaultValues.senses || []} onChange={onChangeSense} isMultiple={true} />
             </div>
 
         </>
