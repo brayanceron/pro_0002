d02c5453df587083e6e8fa979fd3816673b183ba
brayanceron <brayan0np@gmail.com>
2026-02-17 11:12:28 -0500

    update BasicOrderingForm.tsx | format code | add random option

--- Frontend/src/components/forms/GenerationForm/BasicOrderingForm.tsx
+++ Frontend/src/components/forms/GenerationForm/BasicOrderingForm.tsx
@@ -1,7 +1,7 @@
 
 import { useState, useContext, type BaseSyntheticEvent, } from "react";
 import { PlayListContext } from "../../../context/PlayListContext";
-import { type SongModalExtended, type ExtendedGeneric } from "../../../models/SongModel";
+import { type SongModelExtended, type ExtendedGeneric } from "../../../models/SongModel";
 import { PlayListModal, type PlayListModalProps } from "../../../routes/song/generate/PlayListModal";
 import { HSOverlay } from "flyonui/flyonui";
 
@@ -14,40 +14,24 @@ export const BasicOrderingForm = () => {
     const [modalData, setModalData] = useState<PlayListModalProps>({ result: playList, isLoading: false, error: null, res: null, generatedBy: null });
     const [desc, setDesc] = useState<boolean>(true);
 
-    const onChangeRadio = (value: selectedCriterianValues) => { setSelectedCriterian(value); }
-
+    const onChangeRadio = (value: selectedCriterianValues) => setSelectedCriterian(value);
+    const OnClickBtnRandom = () => setNewPlayList(shuffleArray(playList));
     const OnClickBtnApply = () => {
         if (!selectedCriterian) return alert("Not criterian selected");
-        setModalData({ result: OrderPlayList(), isLoading: false, error: null, res: null, generatedBy });
+        setNewPlayList(OrderPlayList(playList, desc, selectedCriterian));
+    }
+
+    const setNewPlayList = (newList : SongModelExtended[]) => {
+        if(!newList) return alert("Error with new list")
+        setModalData({ result: newList, isLoading: false, error: null, res: null, generatedBy });
         HSOverlay.open('#large-modal'); // modal.open()
     }
 
-    const OrderPlayList = () => {
-        const newLista: SongModalExtended[] = [...playList];
-
-        newLista.sort((first: SongModalExtended, second: SongModalExtended) => {
-            const v1 = !desc ? first[selectedCriterian as keyof SongModalExtended] : second[selectedCriterian as keyof SongModalExtended]
-            const v2 = !desc ? second[selectedCriterian as keyof SongModalExtended] : first[selectedCriterian as keyof SongModalExtended]
-
-            if (typeof v1 === "string" && typeof v2 === "string") return v1.toString().localeCompare(v2.toString());
-            else if (typeof v1 === "number" && typeof v2 === "number") return parseFloat(v1) - parseFloat(v2);
-            else if (Array.isArray(v1) && Array.isArray(v2)) {
-                const _v1 = [...v1];
-                const _v2 = [...v2];
-                _v1.sort((f: ExtendedGeneric, s: ExtendedGeneric) => { return (f.name).localeCompare((s.name)) }); // else if( 'name' in x && 'name' in xx) {
-                _v2.sort((f: ExtendedGeneric, s: ExtendedGeneric) => { return (f.name).localeCompare((s.name)) });
-
-                if (!_v1[0] || !_v2[0]) return 0;
-                return _v1[0].name.localeCompare(_v2[0].name);
-            }
-            else return 0;
-        });
-
-        return newLista;
-    }
-
     return (
-        <>
+        <div className="flex gap-1 justify-end align-middle items-center">
+            <button onClick={OnClickBtnRandom} className="badge badge-neutral size-6 mr-1 mt-1 p-0 btn btn-xs btn-secondary">
+                <span className="icon-[tabler--arrows-shuffle]"></span>
+            </button>
             <OrderIndicatorButton initState={desc} setState={setDesc} onClick={OnClickBtnApply} />
 
             <div className="join drop-shadow checked:bg-red-700">
@@ -66,8 +50,8 @@ export const BasicOrderingForm = () => {
                 }
             </div>
             <PlayListModal {...modalData} />
-        </>
-    )
+        </div>
+    );
 }
 
 const OrderIndicatorButton = ({ initState, setState, onClick }: { initState: boolean, setState: (newState: boolean) => void, onClick: () => void }) => {
@@ -89,4 +73,38 @@ const OrderIndicatorButton = ({ initState, setState, onClick }: { initState: boo
 
         </div>
     );
+}
+
+
+function shuffleArray<T>(array: T[]): T[] {
+    const shuffledArray = [...array];
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]]; // Intercambiamos el elemento actual (en i) con el elemento aleatorio (en j)
+    }
+    return shuffledArray;
+}
+
+const OrderPlayList = ( pl : SongModelExtended[], desc: boolean, selectedCriterian: selectedCriterianValues) => {
+    const newLista: SongModelExtended[] = [...pl];
+
+    newLista.sort((first: SongModelExtended, second: SongModelExtended) => {
+        const v1 = !desc ? first[selectedCriterian as keyof SongModelExtended] : second[selectedCriterian as keyof SongModelExtended]
+        const v2 = !desc ? second[selectedCriterian as keyof SongModelExtended] : first[selectedCriterian as keyof SongModelExtended]
+
+        if (typeof v1 === "string" && typeof v2 === "string") return v1.toString().localeCompare(v2.toString());
+        else if (typeof v1 === "number" && typeof v2 === "number") return parseFloat(v1) - parseFloat(v2);
+        else if (Array.isArray(v1) && Array.isArray(v2)) {
+            const _v1 = [...v1];
+            const _v2 = [...v2];
+            _v1.sort((f: ExtendedGeneric, s: ExtendedGeneric) => { return (f.name).localeCompare((s.name)) }); // else if( 'name' in x && 'name' in xx) {
+            _v2.sort((f: ExtendedGeneric, s: ExtendedGeneric) => { return (f.name).localeCompare((s.name)) });
+
+            if (!_v1[0] || !_v2[0]) return 0;
+            return _v1[0].name.localeCompare(_v2[0].name);
+        }
+        else return 0;
+    });
+
+    return newLista;
 }
