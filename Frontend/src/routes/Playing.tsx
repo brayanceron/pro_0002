e71a404b33439fead23a4bbc0fbb21512cab939d
brayanceron <brayan0np@gmail.com>
2026-02-17 10:30:34 -0500

    update/add OptionsPanel

--- Frontend/src/routes/Playing.tsx
+++ Frontend/src/routes/Playing.tsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { PlayerComponent } from "../components/player/PlayerComponent";
 import { PlayListContext } from "../context/PlayListContext";
 import SongList from "../components/player/SongList";
+import { BasicOrderingForm } from "../components/forms/GenerationForm/BasicOrderingForm";
 
 const Playing = () => {
     const { playList, isLoading, error, currentIndex } = useContext(PlayListContext);
@@ -16,7 +17,7 @@ const Playing = () => {
                             <div className="bg-black">
                                 <PlayerComponent /> {/* <PlayerComponent songs={["https://music.youtube.com/watch?v=w9jrC_oJQNM"]} /> */}
                             </div>
-                            
+                            <OptionsPanel />
                             <div className="h-[350px] overflow-y-scroll">
                                 <SongList playList={playList} isLoading={isLoading} error={error} currentIndex={currentIndex} />
                             </div>
@@ -26,6 +27,18 @@ const Playing = () => {
     );
 }
 
+const OptionsPanel = () => {
+    const { playList, currentIndex } = useContext(PlayListContext)
+    
+    return (
+        <div className="flex gap-1 justify-end align-middle items-center">
+            <div className="mr-2 mt-1">
+                <p className="text-[10px] text-gray-400 mx-2">Songs: {currentIndex}{"/"}{playList.length}</p>
+            </div>
+            <BasicOrderingForm />{/* <OrderingForm/> */}
+        </div>
+    )
+}
 
 
 export default Playing
