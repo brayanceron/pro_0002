c64747d675296d4902444fa676bd98bbc5ec259a
brayanceron <brayan0np@gmail.com>
2026-02-25 10:43:09 -0500

    add defaultImages.ts

---
+++ Frontend/src/utils/defaultImages.ts
@@ -0,0 +1,21 @@
+import gender from "../assets/default/gender.png"
+import language from "../assets/default/language.png"
+import playlist from "../assets/default/playlist.png"
+import sense from "../assets/default/sense.png"
+import singer from "../assets/default/singer.jpg"
+import song from "../assets/default/song.png"
+
+export const defaultImages: any = {
+    gender,
+    language,
+    playlist,
+    sense,
+    singer,
+    song,
+    genders: gender,
+    languages: language,
+    playlists: playlist,
+    senses: sense,
+    singers: singer,
+    songs: song,
+}
