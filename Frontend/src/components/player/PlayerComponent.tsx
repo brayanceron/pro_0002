1e01540e757465044d1b09e47174369869427884
brayanceron <brayan0np@gmail.com>
2026-03-24 22:21:30 -0500

    update PlayerComponent.tsx

--- Frontend/src/components/player/PlayerComponent.tsx
+++ Frontend/src/components/player/PlayerComponent.tsx
@@ -6,6 +6,7 @@ import { getUrlBySong, isValidYouTubeUrl } from "../../utils/urls"
 import './style.css'
 import { ProgressBarPlayer } from "./ProgressBarPlayer";
 import type { SongModelExtended } from "../../models/SongModel";
+import { PlayerControls } from "./PlayerControls";
 
 type Props = {
     playList? : SongModelExtended[],
@@ -122,64 +123,12 @@ const PlayerComponent = ({ playList, currentIndex, setCurrentIndex, onChangeStat
 
 
             {/* CONTROLS SECTION */}
-            <div className={`w-full mt-0 mb-2 ${showControls ? '' : 'hidden'} ${(playerSelected && playerSelected.error) ? 'hidden' : ''} ${songs.length === 0 ? 'hidden' : ''}`}>
-                <PlayerControls play={play} pause={pause} move={move} onChangeVolume={onChangeVolume} initVol={initVol} srcLink={songs[currentIndex]} />
+            <div className={`w-full mt-0 mb-2 ${showControls ? '' : 'hidden'} ${(playerSelected && playerSelected.error) ? 'cursor-not-allowed' : ''} ${songs.length === 0 ? 'hidden' : ''}`}>
+                <PlayerControls playing={playing} play={play} pause={pause} move={move} onChangeVolume={onChangeVolume} initVol={initVol} srcLink={songs[currentIndex]} />
             </div>
         </div>
     )
 }
 
-
-const PlayerControls = ({play, pause, move,  onChangeVolume, initVol, srcLink = '/'}:{play : () => void, pause : () => void, move : (step: number) => void, initVol: number, onChangeVolume : (value: number) => void , srcLink? : string}) => {
-    const onChangeInputVolume = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const value = parseInt(e.target.value);
-        if(isNaN(value)) return alert("Please enter a valid number for volume.");
-        onChangeVolume(value);
-    }
-
-    return (
-        <div className="flex items-center justify-between">
-
-                <div className="flex-1 flex justify-start">
-                    <a href={srcLink} target="_blank">
-                        <span className="icon-[tabler--external-link] size-4 invert m-0 p-0"></span>
-                    </a>
-                </div>
-
-                <div style={{flex : '0 0 auto'}}>
-                <button className="btn btn-circle btn-soft btn-default glass text-white" aria-label="Circle Soft Icon Button">
-                    <span className="icon-[tabler--repeat]"></span>
-                </button>
-
-                <button onClick={ _ => move(-1)} className="btn btn-circle btn-soft btn-default glass text-white" aria-label="Circle Soft Icon Button">
-                    <span className="icon-[tabler--track-prev]"></span>
-                </button>
-
-                <button onClick={pause} className="btn btn-circle btn-soft btn-default glass text-white" aria-label="Circle Soft Icon Button">
-                    <span className="icon-[tabler--pause]"></span>
-                </button>
-
-                <button onClick={play} className="btn btn-circle btn-soft btn-default glass text-white" aria-label="Circle Soft Icon Button">
-                    <span className="icon-[tabler--play]"></span>
-                </button>
-
-                <button onClick={ _ => move(1)} className="btn btn-circle btn-soft btn-default glass text-white" aria-label="Circle Soft Icon Button">
-                    <span className="icon-[tabler--track-next]"></span>
-                </button>
-                </div>
-
-                <div className="flex-1">
-                    <div className="w-auto flex items-center gap-1 justify-end mx-1">
-                        <span className="icon-[tabler--volume] size-4 invert"></span>
-                        <input type="range" className="range range-xs w-[60px] bg-transparent border-solid border-white border-[1px]" aria-label="range" defaultValue={initVol} onChange={onChangeInputVolume} />
-                    </div>
-                </div>
-
-                {/* <div>
-                    <span className="icon-[tabler--brand-youtube-filled] size-4 invert"></span>
-                </div> */}
-            </div>
-    )
-}
 
 export { PlayerComponent }
